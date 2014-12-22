b3e5158eda61d8299dcd10a78dbf5cfa88afc200
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2014-12-22 16:41:05 -0500

    Refactoring into functions

--- WebContent/js/tanks.js
+++ WebContent/js/tanks.js
@@ -4,19 +4,27 @@
 
 "use strict";
 
-function drawTank() {
-    var canvas = document.getElementById("tankCanvas");
-    var context = canvas.getContext('2d'); 
+var canvas = document.getElementById("tankCanvas");
+var context = canvas.getContext('2d'); 
+
+function drawAll() {
+    drawCaptureCircle();
+    drawTank();
+}
+
+function drawCaptureCircle() {
     context.beginPath();
     context.arc(95, 50, 40, 0, 2 * Math.PI);
     context.stroke();
-    
+}
+
+function drawTank() {
     var imageScale = 0.20;
     
     var tank1Image = new Image();
     tank1Image.src = 'images/tank1imagev2.png';
     context.save();
-    context.translate(200 * imageScale, 200 * imageScale);
+    context.translate(500 * imageScale, 500 * imageScale);
     context.scale(imageScale, imageScale);
     context.rotate(1.0);
     context.translate( -tank1Image.width * imageScale / 2, -tank1Image.height * imageScale / 2);
@@ -24,4 +32,4 @@ function drawTank() {
     context.restore();
 }
 
-drawTank();
+drawAll();
