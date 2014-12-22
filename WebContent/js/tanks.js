7e32b40b06829742e990211173f854d98bb9ee99
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2014-12-22 16:59:51 -0500

    Tank moving on key press

--- WebContent/js/tanks.js
+++ WebContent/js/tanks.js
@@ -5,12 +5,16 @@
 "use strict";
 
 var canvas = document.getElementById("tankCanvas");
-var context = canvas.getContext('2d'); 
+var context = canvas.getContext('2d');
+var imageScale = 0.20;
 
-function drawAll() {
-    drawCaptureCircle();
-    drawTank();
-}
+var tank1 = {
+    x: 500,
+    y: 500,
+    bodyRotation: 1.0,
+    turretRotation: 1.0,
+    bodyImage: new Image()
+};
 
 function drawCaptureCircle() {
     context.beginPath();
@@ -18,18 +22,45 @@ function drawCaptureCircle() {
     context.stroke();
 }
 
-function drawTank() {
-    var imageScale = 0.20;
+function drawTank(tank) {
+    var bodyImage = tank.bodyImage;
     
-    var tank1Image = new Image();
-    tank1Image.src = 'images/tank1imagev2.png';
     context.save();
-    context.translate(500 * imageScale, 500 * imageScale);
+    context.translate(tank.x * imageScale, tank.y * imageScale);
     context.scale(imageScale, imageScale);
     context.rotate(1.0);
-    context.translate( -tank1Image.width * imageScale / 2, -tank1Image.height * imageScale / 2);
-    context.drawImage(tank1Image, 0, 0);
+    context.translate( -bodyImage.width * imageScale / 2, -bodyImage.height * imageScale / 2);
+    context.drawImage(bodyImage, 0, 0);
     context.restore();
 }
 
+function keypress(event) {
+    var tank = tank1;
+   console.log("keypress event detected: ", event);
+   if (event.key === 'a' || event.key === 'A') {
+       tank.x = tank.x - 10;
+   } else if (event.key === 'd' || event.key === 'D') {
+       tank.x = tank.x + 10;
+   } else if (event.key === 'w' || event.key === 'W') {
+       tank.y = tank.y - 10;
+   } else if (event.key === 's' || event.key === 'S') {
+       tank.y = tank.y + 10;
+   }
+   drawAll();
+}
+
+function drawAll() {
+    context.clearRect ( 0 , 0 , canvas.width, canvas.height );
+    drawCaptureCircle();
+    drawTank(tank1);
+}
+
+function setup() {
+
+    tank1.bodyImage.src = 'images/tank1imagev2.png';
+    
+    window.onkeypress = keypress;
+}
+
+setup();
 drawAll();
